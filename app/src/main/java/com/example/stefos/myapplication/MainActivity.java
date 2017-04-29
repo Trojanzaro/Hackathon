0b5462bd9c220a0c4910588976f90a9e7879e539
stefos96 <stefos96@hotmail.com>
2017-04-29 15:33:58 +0300

    gps

--- app/src/main/java/com/example/stefos/myapplication/MainActivity.java
+++ app/src/main/java/com/example/stefos/myapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.stefos.myapplication;
-
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +11,52 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button b1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b1 = (Button) findViewById(R.id.button);
         b1.setOnClickListener(this);
+
+        // Calls method that handles geolocation
+        this.locListener();
     }
+
+
+
+    // Handles network location
+    private void locListener() {
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+
+            // Called when a new location is found by the network location provider.
+            public void onLocationChanged(Location location) {
+                // TODO: IMPLEMENTATIONS PLS
+                // makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        try {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+        catch (SecurityException e){}
+
+    }
+
+
+
+
 
     @Override
         public void onClick(View v) {
@@ -24,4 +65,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 }
+
 
